278cce3277ec384aa88f0e90e997b0cadb96f508
alonsjous <73393918+alonsjous@users.noreply.github.com>
2021-05-12 20:15:00 -0500

    Hart meeting: Segmented table
    Compiles and succesfully subscribes to the robot's point cloud, and segments everything but the plane that is the table. Still needs to be converted to a sensor_msgs::PointCloud2 and published. We also need to put coke cans in the sim and segment those out as well. We used tutorials from: https://pcl.readthedocs.io/projects/tutorials/en/latest/planar_segmentation.html , https://answers.ros.org/question/136916/conversion-from-sensor_msgspointcloud2-to-pclpointcloudt/ , https://pointclouds.org/documentation/tutorials/planar_segmentation.html

---
+++ src/project/src/main.cpp
@@ -0,0 +1,93 @@
+#include <ros/ros.h>
+// PCL specific includes
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
+// from website
+#include <pcl/conversions.h>
+#include <pcl_ros/transforms.h>
+#include <pcl/PCLPointCloud2.h>
+
+// from reys website
+#include <pcl/ModelCoefficients.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+
+// from harts website
+#include <iostream>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/extract_indices.h>
+
+ros::Publisher pub;
+
+void 
+cloud_cb (const boost::shared_ptr<const sensor_msgs::PointCloud2>& input)
+{
+    pcl::PCLPointCloud2 cloud;
+    pcl_conversions::toPCL(*input, cloud);
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::fromPCLPointCloud2(cloud,*temp_cloud);
+
+
+    pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
+    pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+
+    // Create the segmentation object
+    pcl::SACSegmentation<pcl::PointXYZ> seg;
+
+    // Optional
+    seg.setOptimizeCoefficients (true);
+
+    // Mandatory
+    seg.setModelType (pcl::SACMODEL_PLANE);
+    seg.setMethodType (pcl::SAC_RANSAC);
+    seg.setDistanceThreshold (0.01);
+
+    seg.setInputCloud (temp_cloud);
+    seg.segment (*inliers, *coefficients);
+
+    pcl::ExtractIndices<pcl::PointXYZ> extract;
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr table_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+
+    extract.setInputCloud (temp_cloud);
+    extract.setIndices (inliers);
+    extract.setNegative (false);
+    extract.filter (*table_cloud);
+
+
+  // Create a container for the data.
+  sensor_msgs::PointCloud2 output;
+
+
+
+  // Do data processing here...
+  output = *input;
+  ROS_INFO("Cloud callback");
+
+  // Publish the data.
+  pub.publish (output);
+}
+
+int
+main (int argc, char** argv)
+{
+  // Initialize ROS
+  ros::init (argc, argv, "my_pcl_tutorial");
+  ros::NodeHandle nh;
+
+  // Create a ROS subscriber for the input point cloud
+  ros::Subscriber sub = nh.subscribe ("/hsrb/head_rgbd_sensor/depth_registered/rectified_points", 1, cloud_cb);
+
+  // Create a ROS publisher for the output point cloud
+  pub = nh.advertise<sensor_msgs::PointCloud2> ("output", 1);
+
+  // Spin
+  ros::spin ();
+}
